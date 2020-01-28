86e15587915b394bb239197fa52fbc29181b1b3e
anusha1018 <janardhanaanusha407@gmail.com>
2020-01-28 13:40:37 +0530

    checked exception

---
+++ CheckedException/src/ctsdemo1/CheckedException.java
@@ -0,0 +1,14 @@
+package ctsdemo1;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class CheckedException 
+{
+public static void main(String arg[])
+{
+	FileInputStream
+
+
+}
+}
